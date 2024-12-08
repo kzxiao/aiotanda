c9e9d6be758e39c89a03440ac542e8431d856e05
kzx <embeddednode@gmail.com>
2024-12-08 18:35:04 +0800

    fixed a little bug.

--- hw5/main3.py
+++ hw5/main3.py
@@ -19,7 +19,7 @@ def load_cifar10():
     # https://pytorch.org/vision/main/models/generated/torchvision.models.vgg16.html#torchvision.models.VGG16_Weights
     # https://pytorch.org/vision/main/models/generated/torchvision.models.vgg16.html#torchvision.models.VGG16_Weights
     # transform = transforms.Compose([
-    #     transforms.Resize((224, 224)),
+    #     transforms.Resize((256, 256)),
     #     # transforms.CenterCrop((224, 224)),
     #     transforms.ToTensor(),
     #     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
